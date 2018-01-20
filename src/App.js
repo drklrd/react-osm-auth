d694d4ccd3c9c38ff0cd2c9025552006facbb05f
drklrd <srvbhattarai@gmail.com>
2018-01-20 20:20:24 +0545

    basic working module

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import osmAuth from 'osm-auth';
+
 import './App.css';
 
+let _osmAuthInstance = {};
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    _osmAuthInstance = osmAuth({
+          url : 'https://master.apis.dev.openstreetmap.org',
+          oauth_consumer_key: 'jidyjmlqfYaVXJzhSdEsbc6k5yGsbsWAZCfbz4Rs',
+          oauth_secret: '9Q1GAeFNz5FysQhyAr3p076j3RXlQa4S3VpjY11w',
+          auto: true,
+    });
+    this.authenticate = this.authenticate.bind(this);
+    this.logout = this.logout.bind(this);
+    this.state = {
+        authenticated : _osmAuthInstance.authenticated()
+    };
+  }
+
+  authenticate(){
+      _osmAuthInstance.xhr({
+          method: 'GET',
+          path: '/api/0.6/user/details'
+      },(err,details)=>{
+          if(!err) this.setState({
+              authenticated : true
+          });
+      })
+  }
+
+  logout(){
+    _osmAuthInstance.logout();
+    this.setState({
+       authenticated : false
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello</h1>
+          {
+              this.state.authenticated &&
+              <button onClick={this.logout}>Logout</button>
+
+          }
+          {
+              !this.state.authenticated &&
+              <button onClick={this.authenticate}>Login</button>
+
+          }
+
       </div>
     );
   }
