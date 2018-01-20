f349fb92632d0089215fb1d256d36bc6094baf27
drklrd <srvbhattarai@gmail.com>
2018-01-20 21:52:46 +0545

    update README and remove unnecessary codes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Component } from 'react';
+import osmAuth from 'osm-auth';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let _osmAuthInstance = {};
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        _osmAuthInstance = osmAuth(props);
+        this.authenticate = this.authenticate.bind(this);
+        this.logout = this.logout.bind(this);
+        this.state = {
+            authenticated : _osmAuthInstance.authenticated()
+        };
+    }
+
+    authenticate(){
+        _osmAuthInstance.xhr({
+            method: 'GET',
+            path: '/api/0.6/user/details'
+        },(err,details)=>{
+            if(!err) this.setState({
+                authenticated : true
+            });
+        })
+    }
+
+    logout(){
+        _osmAuthInstance.logout();
+        this.setState({
+            authenticated : false
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={()=>this.state.authenticated ? this.logout() : this.authenticate()}> {this.state.authenticated ? 'Logout' : 'Login'} </button>
+            </div>
+        );
+    }
+}
+
+export default App;
